83115d8aa24a734d012769a14d7e3ee211a05929
Harshil-2193 <harshil12497@gmail.com>
2025-06-12 11:05:42 +0530

    Setup Redux Toolkit with auth slice, store configuration, and login/logout actions

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,42 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
-
+import {useDispatch} from "react-redux"
+import authService from "./appwrite/auth";
+import { login, logout } from './store/authSlice';
+import {Header, Footer} from "./components"
+import {Outlet} from "react-router-dom";
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-   <>
-    <h1>Heloo jee</h1>
-   </>
-  )
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then(userData => {
+      if(userData)
+        dispatch(login({userData}));
+      else
+        dispatch(logout());
+    })
+    .finally(()=>setLoading(false))
+  }, []);
+
+
+    return !loading ? (
+      <div className='min-h-screen flex flex-wrap content-between bg-gray-500'>
+          <div className='w-full block'>
+          <Header/>
+          <main>
+            <Outlet/>
+          </main>
+          <Footer/> 
+        </div>
+      </div>
+    ) : (
+      <div className="h-screen flex items-center justify-center bg-gray-500">
+        <p className="text-white text-xl font-semibold">Loading...</p>
+      </div>
+    );
 }
 
 export default App
